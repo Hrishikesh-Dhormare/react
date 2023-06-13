cddf611d315c746e5172b63182e08671fab860c7
Hrishikesh <dhormarehrishi10@gmail.com>
2023-06-13 15:59:47 +0530

    styling using external css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 function App() {
   let str ="Student of CDAC"
   let user= { id:7, name:"Hrishikesh", class:"PG-DAC"}
-
+  let user2 ={id:8, name:"Mayur", class:"DBDA"}
   return(
     <>
-      <h1>{str}</h1>
+      <h1 style={{color:"red"}}>{str}</h1>
       <h3>ID:{user.id}</h3>
       <h3>Name:{user.name}</h3>
       <h3>Class:{user.class}</h3>
+      <br />
+      <h3>ID:{user2.id}</h3>
+      <h3>Name:{user2.name}</h3>
+      <h3>Class:{user2.class}</h3>
     </>
   );
 }
