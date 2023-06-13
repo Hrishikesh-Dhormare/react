98c3497d25bb0f8af38433e952058cb184153373
Hrishikesh <dhormarehrishi10@gmail.com>
2023-06-13 15:33:48 +0530

    Variable-binding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 function App() {
   let str = "Basic variable binding"
   let pi= 3.14;
-  let active = true;
+  let isUserLogin = true;
 
 
   return(
     <>
       <h1>{str}</h1>
       <h2>{pi}</h2>
-      <h3>{!active? "True": "False"}</h3>
+      <h3>{isUserLogin && <h1>Welcome CDAC</h1>}</h3>
+      <h3>{isUserLogin && <p>Try Again</p>}</h3>
     </>
   )
 }
